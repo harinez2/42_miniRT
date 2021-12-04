52b84dcf416443d93e410543cc19097ff2b14501
harinez2 <yonishi@student.42tokyo.jp>
2021-12-04 17:57:49 +0900

    update debug print

--- minirt/srcs/util_print_m.c
+++ minirt/srcs/util_print_m.c
@@ -31,19 +31,19 @@ static void	print_m_eye(t_map *m)
 	printf(" / orien: %.2f, %.2f, %.2f / fov:%.2f\n\n",
 		m->curr_cam.orien.x, m->curr_cam.orien.y, m->curr_cam.orien.z,
 		m->curr_cam.fov);
-	printf("  distance_cam_scr: %.2f\n", m->scr.distance_cam_scr);
+	printf("  distance_cam_scr : %.2f\n", m->scr.distance_cam_scr);
 	ft_vecprint_with_name("  unitx ", &m->scr.unitvec_scrx);
 	ft_vecprint_with_name("  unity ", &m->scr.unitvec_scry);
 	ft_vecprint_with_name("\n  v_w   ", &m->camdir.v_w);
 	ft_vecprint_with_name("  v_de  ", &m->camdir.v_de);
 	ft_vecprint_with_name("  v_tpos", &m->camdir.v_tpos);
-	printf("\n");
 }
 
 static void	print_m_light(t_map *m)
 {
 	int	i;
 
+	printf("\n  lit_cnt  : %d\n", m->lit_cnt);
 	i = 0;
 	while (i < m->lit_cnt)
 	{
@@ -60,7 +60,7 @@ static void	print_m_obj(t_map *m)
 {
 	int	i;
 
-	printf("\n  obj_count: %d\n", m->obj_count);
+	printf("\n  obj_count   : %d\n", m->obj_count);
 	i = 0;
 	while (i < m->obj_count)
 	{
