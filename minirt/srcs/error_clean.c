2b7c69713dcda898619fbbdb961259fe360aface
harinez2 <yonishi@student.42tokyo.jp>
2021-12-05 14:01:25 +0900

    changed error message

--- minirt/srcs/error_clean.c
+++ minirt/srcs/error_clean.c
@@ -28,7 +28,7 @@ static void	print_error_msg(int errNo)
 	if (errNo == ERR_SYS_MALLOC)
 		printf("Error\nFailed to allocate memory.\n");
 	else if (errNo == ERR_SYS_FILENAME)
-		printf("Error\nFile extension is not '.rt'.\n");
+		printf("Error\nUnrecognied parameter. File extension should be '.rt'.\n");
 	else if (errNo == ERR_SYS_FILEOPEN)
 		printf("Error\nFailed to open file.\n");
 	else if (errNo == ERR_SYS_FILEWRITE)
