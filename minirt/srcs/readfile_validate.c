5467c162e066caca3354bbb1139b8498ae8e7323
harinez2 <yonishi@student.42tokyo.jp>
2021-12-11 18:35:55 +0900

    added resolution size chk

--- minirt/srcs/readfile_validate.c
+++ minirt/srcs/readfile_validate.c
@@ -2,13 +2,8 @@
 
 static void	validate_ACL(t_map *m)
 {
-	// if (m->window_x <= 0 || INT_MAX < m->window_x)
-	// 	print_error_exit(ERR_RD_OUTOFRANGE, m);
-	// if (m->window_y <= 0 || INT_MAX < m->window_y)
-	// 	print_error_exit(ERR_RD_OUTOFRANGE, m);
 	if (m->ambItsty == -1)
 		print_error_exit(ERR_CHK_NO_A, m);
-
 }
 
 static void	validate_objs(t_map *m)
