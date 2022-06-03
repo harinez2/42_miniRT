b1db86213a4488ac502bb2948add522f26f682fe
harinez2 <yonishi@student.42tokyo.jp>
2022-06-03 21:52:38 +0900

    fixed to calculate t for a cylinder object

--- minirt/srcs/obj_cylinder_getdistance.c
+++ minirt/srcs/obj_cylinder_getdistance.c
@@ -15,45 +15,63 @@ static void	calc_ABCD(
 	cv->D = cv->B * cv->B - 4 * cv->A * cv->C;
 }
 
+static void	calc_cylinder_t(
+	t_vec v_w, t_map *m, t_cylinder *tc, t_calc_crossing *cv)
+{
+	t_multdouble	md1;
+	t_multdouble	md2;
+	t_multdouble	md3;
+
+	cv->v_de = ft_vecsub(v_w, m->curr_cam.pos);
+	md1.a = m->curr_cam.pos.x - tc->center.x;
+	md1.b = m->curr_cam.pos.y - tc->center.y;
+	md1.c = m->curr_cam.pos.z - tc->center.z;
+	md2.a = tc->orientation.y * cv->v_de.z - tc->orientation.z * cv->v_de.y;
+	md2.b = tc->orientation.z * cv->v_de.x - tc->orientation.x * cv->v_de.z;
+	md2.c = tc->orientation.x * cv->v_de.y - tc->orientation.y * cv->v_de.x;
+	md3.a = tc->orientation.y * md1.c - tc->orientation.z * md1.b;
+	md3.b = tc->orientation.z * md1.a - tc->orientation.x * md1.c;
+	md3.c = tc->orientation.x * md1.b - tc->orientation.y * md1.a;
+	calc_ABCD(cv, &md2, &md3, tc);
+}
+
 static double	check_cylinder_length(
-	t_map *m, t_cylinder *tc, t_calc_crossing cv)
+	t_map *m, t_cylinder *tc, t_calc_crossing cv, double t)
 {
 	t_vec		v_tpos;
 	t_vec		v_po_p;
 	double		v_po_p_len;
 
-	v_tpos = ft_vecadd(m->curr_cam.pos, ft_vecmult(cv.v_de, cv.t));
+	v_tpos = ft_vecadd(m->curr_cam.pos, ft_vecmult(cv.v_de, t));
 	v_po_p = ft_vecsub(v_tpos, tc->center);
 	v_po_p_len = ft_vecnorm(v_po_p);
 	if (tc->diameter * tc->diameter + tc->height * tc->height / 4
 		< v_po_p_len * v_po_p_len)
 		return (-1);
-	return (0);
+	return (t);
 }
 
 // tpos：cross point (pi) of the v_cam and the surface of the cylinder
 double	get_distance_to_cylinder(t_vec v_w, t_map *m, t_cylinder *tc)
 {
 	t_calc_crossing	cv;
-	t_multdouble	md1;
-	t_multdouble	md2;
-	t_multdouble	md3;
+	double			t1;
+	double			t2;
 
-	cv.v_de = ft_vecsub(v_w, m->curr_cam.pos);
-	md1.a = m->curr_cam.pos.x - tc->center.x;
-	md1.b = m->curr_cam.pos.y - tc->center.y;
-	md1.c = m->curr_cam.pos.z - tc->center.z;
-	md2.a = tc->orientation.y * cv.v_de.z - tc->orientation.z * cv.v_de.y;
-	md2.b = tc->orientation.z * cv.v_de.x - tc->orientation.x * cv.v_de.z;
-	md2.c = tc->orientation.x * cv.v_de.y - tc->orientation.y * cv.v_de.x;
-	md3.a = tc->orientation.y * md1.c - tc->orientation.z * md1.b;
-	md3.b = tc->orientation.z * md1.a - tc->orientation.x * md1.c;
-	md3.c = tc->orientation.x * md1.b - tc->orientation.y * md1.a;
-	calc_ABCD(&cv, &md2, &md3, tc);
-	cv.t = calc_minimum_t(cv.A, cv.B, cv.D);
-	if (cv.t <= 0)
-		return (-1);
-	else if (check_cylinder_length(m, tc, cv) == -1)
-		return (-1);
+	calc_cylinder_t(v_w, m, tc, &cv);
+	cv.t = -1;
+	if (-EPSILON < cv.D && cv.D < EPSILON)
+		cv.t = -cv.B / (2 * cv.A);
+	else if (cv.D > 0)
+	{
+		t1 = (-cv.B - sqrt(cv.D)) / (2 * cv.A);
+		t1 = check_cylinder_length(m, tc, cv, t1);
+		t2 = (-cv.B + sqrt(cv.D)) / (2 * cv.A);
+		t2 = check_cylinder_length(m, tc, cv, t2);
+		if (t1 > 0 && t2 > 0)
+			cv.t = fmin(t1, t2);
+		else
+			cv.t = fmax(t1, t2);
+	}
 	return (cv.t);
 }
