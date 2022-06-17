77bece3549e924a9547b3d171ffb7b41aeb8e3f2
harinez2 <yonishi@student.42tokyo.jp>
2022-06-17 22:17:24 +0900

    bug fix for cylinder shadow

--- minirt/srcs/obj_cylinder_getdistance.c
+++ minirt/srcs/obj_cylinder_getdistance.c
@@ -7,25 +7,24 @@ static t_calc_crossing	calc_cylinder_t(
 	t_vec			v_crossprod_d_n;
 	t_vec			v_crossprod_ac_n;
 
-	cv.v_de = ft_vecsub(v_to, v_from);
-	v_crossprod_d_n = ft_veccrossprod(
-		ft_vecnormalize(cv.v_de), ft_vecnormalize(tc->orientation));
+	cv.v_de = ft_vecnormalize(ft_vecsub(v_to, v_from));
+	v_crossprod_d_n = ft_veccrossprod(cv.v_de, ft_vecnormalize(tc->orientation));
 	cv.A = ft_vecnormsq(v_crossprod_d_n);
 	v_crossprod_ac_n = ft_veccrossprod(ft_vecsub(v_from, tc->center), tc->orientation);
 	cv.B = 2 * ft_vecinnerprod(v_crossprod_d_n, v_crossprod_ac_n);
-	cv.C =ft_vecnormsq(v_crossprod_ac_n) - tc->diameter * tc->diameter;
+	cv.C = ft_vecnormsq(v_crossprod_ac_n) - tc->diameter * tc->diameter;
 	cv.D = cv.B * cv.B - 4 * cv.A * cv.C;
 	return (cv);
 }
 
 static double	check_cylinder_length(
-	t_map *m, t_cylinder *tc, t_calc_crossing cv, double t)
+	t_cylinder *tc, t_calc_crossing cv, double t, t_vec v_from)
 {
 	t_vec		v_tpos;
 	t_vec		v_po_p;
 	double		v_po_p_len;
 
-	v_tpos = ft_vecadd(m->curr_cam.pos, ft_vecmult(ft_vecnormalize(cv.v_de), t));
+	v_tpos = ft_vecadd(v_from, ft_vecmult(cv.v_de, t));
 	v_po_p = ft_vecsub(v_tpos, tc->center);
 	if (ft_vecinnerprod(v_po_p, tc->orientation) < 0)
 		return (-1);
@@ -45,19 +44,20 @@ double	get_distance_to_cylinder(
 	double			t2;
 	double			bigger_t;
 
+	(void)m;
 	cv = calc_cylinder_t(v_from, v_to, tc);
 	tc->secondcrosst_flg = 0;
 	if (cv.A == 0 || cv.D < 0)
 		cv.t = -1;
-	else if (cv.D < EPSILON)
+	else if (cv.D == 0)
 		cv.t = -cv.B / (2 * cv.A);
 	else
 	{
 		t1 = (-cv.B - sqrt(cv.D)) / (2 * cv.A);
 		t2 = (-cv.B + sqrt(cv.D)) / (2 * cv.A);
 		bigger_t = fmax(t1, t2);
-		t1 = check_cylinder_length(m, tc, cv, t1);
-		t2 = check_cylinder_length(m, tc, cv, t2);
+		t1 = check_cylinder_length(tc, cv, t1, v_from);
+		t2 = check_cylinder_length(tc, cv, t2, v_from);
 		if (t1 > 0 && t2 > 0)
 			cv.t = fmin(t1, t2);
 		else
